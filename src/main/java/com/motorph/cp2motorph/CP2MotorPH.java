0bd0901f7dbc64bf5213951bac00204be0a9bcd6
kjalcala <alcalakarljimwell@gmail.com>
2024-05-05 19:57:54 +0800

    Finished Profile Tab

---
+++ src/main/java/com/motorph/cp2motorph/CP2MotorPH.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.motorph.cp2motorph;
+
+/**
+ *
+ * @author Administrator
+ */
+public class CP2MotorPH {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
