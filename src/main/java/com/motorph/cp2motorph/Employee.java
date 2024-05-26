ba01b90d3bb9ac205317d6462c5370614b1fec7f
Emmar8 <155600145+Emmar8@users.noreply.github.com>
2024-05-26 07:46:31 -0400

    Add files via upload

--- src/main/java/com/motorph/cp2motorph/Employee.java
+++ src/main/java/com/motorph/cp2motorph/Employee.java
@@ -1,118 +1,60 @@
-/*
- * Click nbfs://nbhost/FileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package com.motorph.cp2motorph;
+package MotorPH;
 
-/**
- *
- * @author kjalcala
- */
 public class Employee {
-    private int employeeNumber;
-    private String lastName;
-    private final String firstName;
-    private String birthday;
-    private String address;
-    private String phoneNumber;
-    private String sssNumber;
-    private String philhealthNumber;
-    private String tinNumber;
-    private String pagibigNumber;
-    private String status;
-    private String position;
-    private String immediateSupervisor;
-    private double basicSalary;
-    private double riceSubsidy;
-    private double phoneAllowance;
-    private double clothingAllowance;
-    private double grossSemiMonthlyRate;
-    private double hourlyRate;
+	
+	int id;
+    String lastName;
+    String firstName;
+    String dateOfBirth;
+    String address;
+    String phoneNumber;
+    String sssNumber;
+    String philHealthNumber;
+    String tin;
+    String pagIbigNumber;
+    String employmentStatus;
+    String jobTitle;
+    String supervisor;
+    String remarks;
+    double basicSalary;
+    double riceSubsidy;
+    double phoneAllowance;
+    double clothingAllowance;
+    double semiMonthlyRate;
+    double hourlyRate;
+    
+    public Employee(int id, String lastName, String firstName, String dateOfBirth, String address, String phoneNumber,
+            String sssNumber, String philHealthNumber, String tin, String pagIbigNumber, String employmentStatus,
+            String jobTitle, String supervisor, String remarks, double basicSalary, double riceSubsidy,
+            double phoneAllowance, double clothingAllowance, double semiMonthlyRate, double hourlyRate) {
+this.id = id;
+this.lastName = lastName;
+this.firstName = firstName;
+this.dateOfBirth = dateOfBirth;
+this.address = address;
+this.phoneNumber = phoneNumber;
+this.sssNumber = sssNumber;
+this.philHealthNumber = philHealthNumber;
+this.tin = tin;
+this.pagIbigNumber = pagIbigNumber;
+this.employmentStatus = employmentStatus;
+this.jobTitle = jobTitle;
+this.supervisor = supervisor;
+this.remarks = remarks;
+this.basicSalary = basicSalary;
+this.riceSubsidy = riceSubsidy;
+this.phoneAllowance = phoneAllowance;
+this.clothingAllowance = clothingAllowance;
+this.semiMonthlyRate = semiMonthlyRate;
+this.hourlyRate = hourlyRate;
 
-
-    public Employee(int employeeNumber, String lastName, String firstName, String birthday, String address, String phoneNumber, String sssNumber, String philhealthNumber, String tinNumber, String pagibigNumber, String status, String position, String immediateSupervisor, String jane_Smith, double basicSalary, double riceSubsidy, double phoneAllowance, double clothingAllowance, double grossSemiMonthlyRate, double hourlyRate) {
-        this.employeeNumber = employeeNumber;
-        this.lastName = lastName;
-        this.firstName = firstName;
-        this.birthday = birthday;
-        this.address = address;
-        this.phoneNumber = phoneNumber;
-        this.sssNumber = sssNumber;
-        this.philhealthNumber = philhealthNumber;
-        this.tinNumber = tinNumber;
-        this.pagibigNumber = pagibigNumber;
-        this.status = status;
-        this.position = position;
-        this.immediateSupervisor = immediateSupervisor;
-        this.basicSalary = basicSalary;
-        this.riceSubsidy = riceSubsidy;
-        this.phoneAllowance = phoneAllowance;
-        this.clothingAllowance = clothingAllowance;
-        this.grossSemiMonthlyRate = grossSemiMonthlyRate;
-        this.hourlyRate = hourlyRate;
-    }
-    
-    
-    public String getLastName(){
-        return lastName;
-    }
-    
-    public String getFirstName() {
-        return firstName;
-    }
-    public void setLastName() {
-        lastName = getLastName();
-    }
-    
-    public String getBirthday() {
-        return birthday;
-    }
-    public int getEmployeeNumber() {
-        return employeeNumber;
-    }
-    public String getAddress() {
-        return address;
-    }
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-    public String getSssNumber() {
-        return sssNumber;
-    }
-    public String getPhilhealthNumber() {
-        return philhealthNumber;
-    }
-    public String getTinNumber() {
-        return tinNumber;
-    }
-    public String getPagibigNumber() {
-        return pagibigNumber;
-    }
-    public String getStatus() {
-        return status;
-    }
-    public String getPosition() {
-        return position;
-    }
-    public String getImmediateSupervisor() {
-        return immediateSupervisor;
-    }
-    public Double getBasicSalary() {
-        return basicSalary;
-    }
-    public Double getRiceSubsidy() {
-        return riceSubsidy;
-    }
-    public Double getPhoneAllowance() {
-        return phoneAllowance;
-    }
-    public Double getClothingAllowance() {
-        return clothingAllowance;
-    }
-    public Double getGrossSemiMonthlyRate() {
-        return grossSemiMonthlyRate;
-    }
-    public Double getHourlyRate() {
-        return hourlyRate;
+}
+    public String[] toArray() {
+        return new String[]{
+                String.valueOf(id), lastName, firstName, dateOfBirth, address, phoneNumber, sssNumber,
+                philHealthNumber, tin, pagIbigNumber, employmentStatus, jobTitle, supervisor, remarks,
+                String.valueOf(basicSalary), String.valueOf(riceSubsidy), String.valueOf(phoneAllowance),
+                String.valueOf(clothingAllowance), String.valueOf(semiMonthlyRate), String.valueOf(hourlyRate)
+        };
     }
 }
